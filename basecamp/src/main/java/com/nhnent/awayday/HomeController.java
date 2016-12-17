6eaf568521c830fc612e930b6e0c937fc6254eed
AwayDay <awayday1108@gmail.com>
2016-12-17 09:55:13 +0900

    트레비스 연동 시험용

--- basecamp/src/main/java/com/nhnent/awayday/HomeController.java
+++ basecamp/src/main/java/com/nhnent/awayday/HomeController.java
@@ -31,7 +31,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("hell o, serverTime", formattedDate );
 		
 		return "home";
 	}
